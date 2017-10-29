a55d0e5691577b9daa4cdff022e5e17db1d9b236
Jonathan Sanfer <sanferjonathan88@gmail.com>
2017-10-29 13:09:04 +0100

    uppladdning av c arbeten

---
+++ C/kap5/uppg5_8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+  int n;
+  _Bool b = true;
+
+  printf("Ange ett tal: ");
+  scanf("%d\n", &n);
+
+  for(int i = 2; i <= n; i++){
+    for(int j = 2; j < i; j++){
+        if((i % j) == 0){
+          b = false;
+        }
+    }
+    if(b == true){
+      printf("%d\n", i);
+    }
+    else {
+      b = true;
+    }
+  }
+  return 0;
+}
