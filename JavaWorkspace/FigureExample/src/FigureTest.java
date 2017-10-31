01f27c03d2fd3084616fcfd10d94277a97b1810b
Jonathan Sanfer <sanferjonathan88@gmail.com>
2017-10-31 09:24:54 +0100

    to bort lite kommentarer i koden

--- JavaWorkspace/FigureExample/src/FigureTest.java
+++ JavaWorkspace/FigureExample/src/FigureTest.java
@@ -9,13 +9,7 @@ public class FigureTest {
 		figs[3] = new Circle(20,20,20);
 
 		for (int index=0; index < figs.length; index++) {
-
-			// clean the following code into more readable forms
-			// using polymorphism (use printOut)
 			System.out.println(figs[index].getTitle());
-			//if (figs[index] instanceof Circle) {
-				//System.out.println("   Diameter: " + ((Circle) figs[index]).diameter());
-			//} 
 			figs[index].printOut();
 		}
 	}
