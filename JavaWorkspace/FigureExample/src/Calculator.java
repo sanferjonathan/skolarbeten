313025852ea5bb55b1007d57a888d83e06386997
Jonathan Sanfer <sanferjonathan88@gmail.com>
2017-10-31 12:53:31 +0100

    la till kalkulator

---
+++ JavaWorkspace/FigureExample/src/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+	interface IntegerMath {
+		int operation (int a, int b);
+	}
+
+	public int operateBinary(int a, int b, IntegerMath op) {
+		return op.operation(a, b);
+	}
+
+	public static void main (String[] args) {
+		Calculator myApp = new Calculator();
+		IntegerMath addition = (a,b) -> a + b;
+		IntegerMath subtraction = (a,b) -> a - b;
+		//IntegerMath division = (new f() { public int k(int a, int b) { return a / b; }});
+
+		System.out.println("40 + 2 = " + myApp.operateBinary(40, 2, addition));
+		System.out.println("20 - 10 = " + myApp.operateBinary(20, 10, subtraction));
+
+		//System.out.println("20 / 10 = " + myApp.operateBinary(40, 2, division);
+
+	}
+}
